2a03a34af4b9a6442d16c3df6d07401a0c840315
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2017-08-26 18:34:25 -0700

    Snapshot JSZM v2.0.2 http://zzo38computer.org/jszm/jszm.js by zzo38

---
+++ example.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const G=require("genasync"); // A package I wrote
+const JSZM=require("./jszm.js"); // version 2
+const readline=require("readline");
+const fs=require("fs");
+
+const In=readline.createInterface({input: process.stdin});
+const Out=process.stdout;
+
+G.defineER(fs,"readFile","readFileG",2);
+G.defineER(fs,"writeFile","writeFileG",3);
+G.defineR(In,"question","questionG",1);
+
+G.run((function*() {
+  var story=yield fs.readFileG(process.argv[2],{});
+  var game=new JSZM(story);
+  game.print=function*(x) {
+    Out.write(x,"ascii");
+  };
+  game.read=function*() {
+    return yield In.questionG("");
+  };
+  game.save=function*(x) {
+    var n,e;
+    Out.write("Save? ","ascii");
+    n=yield In.questionG("");
+    if(!n) return false;
+    try {
+      yield fs.writeFileG(n,new Buffer(x.buffer),{});
+      return true;
+    } catch(e) {
+      return false;
+    }
+  };
+  game.restore=function*() {
+    var n,e;
+    Out.write("Restore? ","ascii");
+    n=yield In.questionG("");
+    if(!n) return null;
+    try {
+      return new Uint8Array(yield fs.readFileG(n,{}));
+    } catch(e) {
+      return null;
+    }
+  };
+  yield*game.run();
+  process.exit(0);
+})());
